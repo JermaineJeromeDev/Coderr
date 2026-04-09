ca8a0e12a16b72fcc6bea2a15393ac14434cbb9c
Jermaine Jérôme Bärwolf <jay.jero1901@gmail.com>
2026-04-09 10:00:23 +0200

    feat: implement registration endpoint with token response and validation tests

---
+++ auth_app/api/serializers.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from rest_framework.authtoken.models import Token
+
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    repeated_password = serializers.CharField(write_only=True)
+    type = serializers.CharField(write_only=True)
+    token = serializers.SerializerMethodField()
+    user_id = serializers.IntegerField(source='id', read_only=True)
+
+    class Meta:
+        model = User
+        fields = [
+            "user_id", "username", "email", "password", 
+            "repeated_password", "type", "token"
+        ]
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def validate(self, data):
+        if data['password'] != data ['repeated_password']:
+            raise serializers.ValidationError({"password": "Passwords must match."})
+        return data 
+    
+    def create(self, validated_data):
+        validated_data.pop('repeated_password')
+        validated_data.pop('type')
+        user = User.objects.create_user(**validated_data)
+        Token.objects.create(user=user)
+        return user
+    
+    def get_token(self, obj):
+        token, _ = Token.objects.get_or_create(user=obj)
+        return token.key
--- auth_app/api/views.py
+++ auth_app/api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
+from django.contrib.auth.models import User
+from .serializers import RegistrationSerializer
 
-# Create your views here.
+
+class RegistrationView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegistrationSerializer
+    permission_classes = [AllowAny]
