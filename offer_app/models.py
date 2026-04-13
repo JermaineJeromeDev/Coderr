64babd004953a6424a4abd4b74aed809a18d3e2d
Jermaine Jérôme Bärwolf <jay.jero1901@gmail.com>
2026-04-13 10:14:14 +0200

    feat: add initial migration and models for Offer and OfferDetail

--- offer_app/models.py
+++ offer_app/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Offer(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE, 
+        related_name="offers"
+    )
+    title = models.CharField(max_length=255)
+    image = models.FileField(upload_to='offers/', blank=True, null=True)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} by {self.username}"
+    
+
+class OfferDetail(models.Model):
+    offer = models.ForeignKey(
+        Offer, 
+        on_delete=models.CASCADE, 
+        related_name="details"
+    )
+    title = models.CharField(max_length=255)
+    revisions = models.IntegerField(default=-1) 
+    delivery_time_in_days = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    features = models.JSONField(default=list)
+
+    def __str__(self):
+        return f"Detail for {self.offer.title}: {self.price}€"
