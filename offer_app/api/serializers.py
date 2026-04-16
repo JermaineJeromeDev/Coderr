d779814bad411e5f0ec0c47d5d6ca1681a4529db
Jermaine Jérôme Bärwolf <jay.jero1901@gmail.com>
2026-04-16 10:34:20 +0200

    feat: enhance Offer serializers and views for detailed offer handling and user permissions

--- offer_app/api/serializers.py
+++ offer_app/api/serializers.py
@@ -5,23 +5,27 @@ from rest_framework import serializers
 from ..models import Offer, OfferDetail
 
 
-class OfferDetailSerializer(serializers.ModelSerializer):
+class OfferDetailDataSerializer(serializers.ModelSerializer):
+    """Vollständige Details für POST/Anzeige nach Erstellung."""
+    class Meta:
+        model = OfferDetail
+        fields = ["id", "title", "revisions", "delivery_time_in_days", "price", "features"]
+
+
+class OfferDetailShortSerializer(serializers.ModelSerializer):
+    """Kurzform (id, url) für die Liste (GET)."""
     url = serializers.SerializerMethodField()
-
-
     class Meta:
         model = OfferDetail
         fields = ["id", "url"]
-
     def get_url(self, obj):
         return f"/offerdetails/{obj.id}/"
 
-
 class OfferSerializer(serializers.ModelSerializer):
-    user_details = serializers.SerializerMethodField()
-    min_price = serializers.SerializerMethodField()
-    min_delivery_time = serializers.SerializerMethodField()
-    details = OfferDetailSerializer(many=True, read_only=True)
+    user_details = serializers.SerializerMethodField(read_only=True)
+    min_price = serializers.SerializerMethodField(read_only=True)
+    min_delivery_time = serializers.SerializerMethodField(read_only=True)
+    details = OfferDetailDataSerializer(many=True)
 
     class Meta:
         model = Offer
@@ -30,13 +34,29 @@ class OfferSerializer(serializers.ModelSerializer):
             "created_at", "updated_at", "details", 
             "min_price", "min_delivery_time", "user_details"
         ]
+        extra_kwargs = {'user': {'read_only': True}}
+
+    def validate_details(self, value):
+        if len(value) != 3:
+            raise serializers.ValidationError("An offer must have exactly 3 details.")
+        return value
+
+    def create(self, validated_data):
+        details_data = validated_data.pop('details')
+        offer = Offer.objects.create(**validated_data)
+        for detail in details_data:
+            OfferDetail.objects.create(offer=offer, **detail)
+        return offer
+
+    def to_representation(self, instance):
+        """Spagat: GET liefert Kurzform, POST/Anzeige liefert Langform."""
+        rep = super().to_representation(instance)
+        if self.context['request'].method == 'GET':
+            rep['details'] = OfferDetailShortSerializer(instance.details.all(), many=True).data
+        return rep
 
     def get_user_details(self, obj):
-        return {
-            "first_name": obj.user.first_name,
-            "last_name": obj.user.last_name,
-            "username": obj.user.username
-        }
+        return {"first_name": obj.user.first_name, "last_name": obj.user.last_name, "username": obj.user.username}
 
     def get_min_price(self, obj):
         return getattr(obj, 'min_price', 0)
--- offer_app/api/views.py
+++ offer_app/api/views.py
@@ -1,10 +1,10 @@
 # 2. Drittanbieter
 from django.db.models import Min
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListCreateAPIView
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.permissions import AllowAny
-from rest_framework.exceptions import ValidationError
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.exceptions import ValidationError, PermissionDenied
 
 # 3. Lokale Importe
 from ..models import Offer
@@ -15,13 +15,18 @@ class OfferPagination(PageNumberPagination):
     page_size_query_param = 'page_size'
 
 
-class OfferListView(ListAPIView):
+class OfferListView(ListCreateAPIView):
     serializer_class = OfferSerializer
     pagination_class = OfferPagination
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     filter_backends = [SearchFilter, OrderingFilter]
     search_fields = ['title', 'description']
     ordering_fields = ['updated_at', 'min_price']
+
+    def perform_create(self, serializer):
+        if self.request.user.type != 'business':
+            raise PermissionDenied("Only business users can create offers.")
+        serializer.save(user=self.request.user)
 
     def get_queryset(self):
         queryset = Offer.objects.annotate(
