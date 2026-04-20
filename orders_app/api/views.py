c5d0275aa1f2f9dfe02943b0c195ab075327891d
Jermaine Jérôme Bärwolf <jay.jero1901@gmail.com>
2026-04-20 11:26:10 +0200

    feat: add CompletedOrderCountView for retrieving the count of completed orders by business user

--- orders_app/api/views.py
+++ orders_app/api/views.py
@@ -54,3 +54,11 @@ class OrderCountView(APIView):
         user = get_object_or_404(User, id=business_user_id, type='business')
         count = Order.objects.filter(business_user=user, status='in_progress').count()
         return Response({"order_count": count})
+    
+
+class CompletedOrderCountView(APIView):
+    def get(self, request, business_user_id):
+        user = get_object_or_404(User, id=business_user_id, type='business')
+        
+        count = Order.objects.filter(business_user=user, status='completed').count()
+        return Response({"completed_order_count": count})
