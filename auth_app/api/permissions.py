f226bfa3a6c3314e6bce5d562dcae8c9d6b9d800
Jermaine Jérôme Bärwolf <jay.jero1901@gmail.com>
2026-04-22 10:16:13 +0200

    feat: enhance permissions logic to include user ownership check

--- auth_app/api/permissions.py
+++ auth_app/api/permissions.py
@@ -1,10 +1,15 @@
 from rest_framework import permissions
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
     
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
+        if isinstance(obj, get_user_model()):
+            return obj == request.user
         owner_field = getattr(obj, 'user', 
                     getattr(obj, 'reviewer', 
                     getattr(obj, 'customer_user', None)))
