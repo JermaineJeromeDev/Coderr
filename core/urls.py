28b0b4b3484f3456bfb5405059a772733b34fd29
Jermaine Jérôme Bärwolf <jay.jero1901@gmail.com>
2026-04-22 15:18:37 +0200

    chore: update URL configuration file with improved documentation and formatting

--- core/urls.py
+++ core/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+"""
+Main URL configuration for the Coderr project.
+"""
+
+# 1. Standard-Library / Django
 from django.contrib import admin
 from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
