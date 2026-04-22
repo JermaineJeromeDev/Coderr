bf258ecb5a99d455068333f43a636434c54b30a2
Jermaine Jérôme Bärwolf <jay.jero1901@gmail.com>
2026-04-22 15:17:23 +0200

    feat: register Review model in admin with custom display fields

--- reviews_app/admin.py
+++ reviews_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Review
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ["business_user", "reviewer", "rating", "created_at"]
