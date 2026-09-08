87aae18e200f66445806257168addcf78f8cee7f
Jermaine Jérôme Bärwolf <jay.jero1901@gmail.com>
2026-09-08 16:27:14 +0200

    feat(config): update API_BASE_URL and STATIC_BASE_URL for production environment; modify startCommand in render.yaml for migrations

--- coderr_frontend/shared/scripts/config.js
+++ coderr_frontend/shared/scripts/config.js
@@ -13,9 +13,13 @@ const isLocalhost =
   window.location.hostname === "localhost" ||
   window.location.hostname === "127.0.0.1";
 
-const API_BASE_URL = isLocalhost ? "http://127.0.0.1:8000/api/" : "/api/";
+const API_BASE_URL = isLocalhost
+  ? "http://127.0.0.1:8000/api/"
+  : "https://coderr-yq2l.onrender.com/api/";
 
-const STATIC_BASE_URL = isLocalhost ? "http://127.0.0.1:8000/" : "/";
+const STATIC_BASE_URL = isLocalhost
+  ? "http://127.0.0.1:8000/"
+  : "https://coderr-yq2l.onrender.com/";
 
 const LOGIN_URL = "login/";
 
